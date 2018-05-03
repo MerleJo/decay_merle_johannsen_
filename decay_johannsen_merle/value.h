c2a0264d31e683a26fd1daf99920954fd2005bd2
MerleJo <36482746+MerleJo@users.noreply.github.com>
2018-05-03 23:42:20 +0300

    Additional Comments

--- decay_johannsen_merle/value.h
+++ decay_johannsen_merle/value.h
@@ -18,13 +18,13 @@ private :
 	double A;												// Concentration of 1,2‑dichlorobenzene on the outlet of the reactor				[mol/m^3]
 	double alpha;											// Reaction yield																[%]
 	double b;												// Compound factor combining construction and flow parameters of the reactor	[s^-1]
-	double k;
-	double T;
+	double k;												// 	Reaction rate constant														[s^-1]
+	double T;												// Temperature																	[K]
 	
 	// input variables with intervals
-	double A0;
-	double t;
-	double um;
+	double A0;												// Concentration of 1,2‑dichlorobenzene on the inlet to the reactor				[mol/m^3]
+	double t;												// Temperature																	[°C]
+	double um;												// Velocity of gas flow in the reactor											[m/s]
 
 	double calculateT(double t);
 	double calculateA(double A0, double k, double b);
